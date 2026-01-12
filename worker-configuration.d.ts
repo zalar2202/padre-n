b09f77d782587e646c68044204d791312477ac6e
Salar Safayi <108089493+zalar2202@users.noreply.github.com>
2026-01-12 19:08:26 +0300

    Change KV namespace example from 'sassa' to 'sub'
    Updated KV namespace example in Env interface.

--- worker-configuration.d.ts
+++ worker-configuration.d.ts
@@ -1,6 +1,6 @@
 interface Env {
 	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: sassa;
+	// MY_KV_NAMESPACE: sub;
 	//
 	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
 	// MY_DURABLE_OBJECT: DurableObjectNamespace;
